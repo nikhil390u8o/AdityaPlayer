7a29f280d6ac514d7b3e84b4100e868264da0738
nikhil390u8o <sangeetasharma1005sa@gmail.com>
2025-08-13 21:39:58 +0530

    Update AdityaHalder.py

--- AdityaHalder.py
+++ AdityaHalder.py
@@ -310,7 +310,7 @@ def chat_admins_only(mystic):
 
 
 async def get_stream_info(query, streamtype):
-    api_url = "http://46.250.243.87:1470/youtube"
+    api_url = "https://spotify-vert-nine.vercel.app/search?q="
     api_key = "oldkey"
     video = True if streamtype.lower() == "video" else False
     params = {"query": query, "video": video, "api_key": api_key}
