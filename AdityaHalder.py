a7bed9f1d9c01ede7ba6a62a058420ac5665c3db
nikhil390u8o <sangeetasharma1005sa@gmail.com>
2025-08-13 16:10:10 +0530

    Update AdityaHalder.py

--- AdityaHalder.py
+++ AdityaHalder.py
@@ -311,7 +311,7 @@ def chat_admins_only(mystic):
 
 async def get_stream_info(query, streamtype):
     api_url = "http://46.250.243.87:1470/youtube"
-    api_key = "1a873582a7c83342f961cc0a177b2b26"
+    api_key = "oldkey"
     video = True if streamtype.lower() == "video" else False
     params = {"query": query, "video": video, "api_key": api_key}
 
